995de12c3e48f79ed5b1eb03d66f97c24b334ecb
Adrian Paul Padre <adrian.padre03@gmail.com>
2026-08-19 17:49:17 +0800

    fix cors_allowed_origins v2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = 'https://jsx-aedz.vercel.app', 'http://localhost:5173', 'http://127.0.0.1:5173',
+CORS_ALLOWED_ORIGINS = 'https://jsx-chi-silk.vercel.app', 'http://localhost:5173', 'http://127.0.0.1:5173',
 
 ROOT_URLCONF = 'backend.urls'
 
